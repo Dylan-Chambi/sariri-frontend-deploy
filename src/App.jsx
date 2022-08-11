b7592668ffb9937822e48eeee43a942583cc16eb
Mauricio Vargas <65517229+MauriVargas17@users.noreply.github.com>
2022-08-10 23:35:08 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import {Route, Routes} from 'react-router-dom';
 import {getPlacesData} from "./api";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
+import GoogleContextProvider from "./context/googleContext";
 
 export default function App() {
     
   return (
+   <GoogleContextProvider>
     <Routes>
       <Route path="/" element={<SignIn />} />
       <Route path="/home" element={<Home />} />
     </Routes>
+   </GoogleContextProvider>
   );
 }
