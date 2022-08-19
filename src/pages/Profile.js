5b30b0790e11a59c909e7226fb8a52351602dff2
camgany <you@example.com>
2022-08-18 21:52:37 -0400

    fix phone number textfield

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -17,6 +17,7 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import Navbar from '../components/Navbar';
 import imagen from '../assets/bolivia2.jpg';
 import { useRef, useState } from 'react';
+import MuiPhoneNumber from 'material-ui-phone-number';
 
 
 function Copyright(props) {
@@ -36,12 +37,15 @@ const theme = createTheme();
 
 export default function Profile() {
     const [readOnly, setReadOnly] = useState(true);
+    const [phoneNumber, setPhoneNumber] = React.useState('');
 
     const handleEditButtonChange = () => {
         setReadOnly(!readOnly);
         setShow((prevState) => !prevState);
     }
-
+    const handlePhoneNumberChange = (value) => {
+        setPhoneNumber(value);
+    }
     const handleSubmit = (event) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
@@ -52,20 +56,22 @@ export default function Profile() {
     };
     const [show, setShow] = useState(false);
     const [errorText, setErrorText] = useState('');
-    const phoneRegex = /^[0-9]+$/;
+    const phoneRegex = /[0-9]/;
     const [disableButton, setDisableButton] = useState(false);
-    
 
-    const handleError= (event) => {
+
+    const handleError = (event) => {
+        const coma = event.target.value.match(phoneRegex);
+        console.log(coma);
         if (event.target.value.match(phoneRegex)) {
             setErrorText('');
             setDisableButton(false);
-        }else {
+        } else {
             setErrorText('Ingrese un numero de telefono valido');
             setDisableButton(true);
         }
-    } 
-        return (
+    }
+    return (
         <div>
             <ThemeProvider theme={theme}>
                 <Grid container component="main" sx={{ height: '100vh' }}>
@@ -140,6 +146,7 @@ export default function Profile() {
                                         <TextField
                                             id="outlined-read-only-input"
                                             defaultValue="Morrow"
+
                                             InputProps={{
                                                 readOnly: readOnly,
                                             }}
@@ -149,6 +156,7 @@ export default function Profile() {
                                 </Box>
                             </Box>
                             <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+
                                 <Typography variant="body1" color="text.secondary" align="left" sx={{ fontWeight: 'bold' }}>
                                     Telefono:
                                 </Typography>
@@ -161,8 +169,22 @@ export default function Profile() {
                                     autoComplete="off"
                                 >
                                     <div>
-
+                                        <MuiPhoneNumber defaultCountry={'bo'} onChange={(value) => { handlePhoneNumberChange(value) }}
+                                            value="+5916556678"
+                                            margin="normal"
+                                            required
+                                            fullWidth
+                                            
+                                            id="phone"
+                                            
+                                            name="phone"
+                                            autoFocus
+                                            InputProps={{
+                                                readOnly: readOnly,
+                                            }}
+                                        />
                                         <TextField
+
                                             id="outlined-read-only-input"
                                             defaultValue="6556678"
                                             type="number"
@@ -174,20 +196,21 @@ export default function Profile() {
                                                 readOnly: readOnly,
                                             }}
                                         />
+
                                     </div>
                                 </Box>
                             </Box>
                             {!show &&
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{ width: 230, mt: 3, mb: 2 }}
-                                onClick={handleEditButtonChange}
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ width: 230, mt: 3, mb: 2 }}
+                                    onClick={handleEditButtonChange}
 
-                            >
-                                EDITAR
-                            </Button>}
+                                >
+                                    EDITAR
+                                </Button>}
                             <Button
                                 href="/home"
                                 type="submit"
